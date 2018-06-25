4759d30f62c5c27f4bf5e2af760fc23476af2d3d
Elson <elson.ji@gmail.com>
2018-06-24 22:54:45 -0400

    feat: create new UI

--- app/src/main/java/com/elsonji/tipcalculator/MainActivity.java
+++ app/src/main/java/com/elsonji/tipcalculator/MainActivity.java
@@ -15,70 +15,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText billTotalEditText = findViewById(R.id.bill_amount_edit_text);
-
-        final TextView tipTextView = findViewById(R.id.tipTextView);
+        //final TextView tipTextView = findViewById(R.id.tipTextView);
 
         final TextView personCountTextView = findViewById(R.id.personCountTextView);
 
-        final TextView billTotalTextView = findViewById(R.id.billTotalTextView);
-
-        final TextView billTotalPerPersonTextView = findViewById(R.id.totalPerPerson);
+       // final TextView billTotalPerPersonTextView = findViewById(R.id.totalPerPerson);
 
         final double[] billTotalAfterTip = new double[1];
 
         final double[] totalBeforeTip = new double[1];
 
-        Button button15Percent = findViewById(R.id.button_15);
-        button15Percent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String amountString = billTotalEditText.getText().toString().trim();
-                if (amountString.length() > 0) {
-                    totalBeforeTip[0] = Double.parseDouble(billTotalEditText.getText().toString().trim());
-                    tipTextView.setText(String.valueOf(totalBeforeTip[0] * 15 / 100));
-                    billTotalAfterTip[0] = totalBeforeTip[0] + totalBeforeTip[0] * 15 / 100;
-                    billTotalTextView.setText(String.valueOf(billTotalAfterTip[0]));
-                    billTotalPerPersonTextView.setText(String.valueOf(billTotalAfterTip[0]));
-                }
-            }
-        });
 
-        Button button18Percent = findViewById(R.id.button_18);
-        button18Percent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (billTotalEditText.getText().length() > 0) {
-                    double totalBeforeTip = Double.parseDouble(billTotalEditText.getText().toString().trim());
-                    tipTextView.setText(String.valueOf(totalBeforeTip * 18 / 100));
-                    billTotalAfterTip[0] = totalBeforeTip + totalBeforeTip * 18 / 100;
-                    billTotalTextView.setText(String.valueOf(totalBeforeTip + totalBeforeTip * 18 / 100));
-                    billTotalPerPersonTextView.setText(String.valueOf(totalBeforeTip + totalBeforeTip * 18 / 100));
-                }
-            }
-        });
-
-        Button button20Percent = findViewById(R.id.button_20);
-        button20Percent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (billTotalEditText.getText().length() > 0) {
-                    double totalBeforeTip = Double.parseDouble(billTotalEditText.getText().toString().trim());
-                    tipTextView.setText(String.valueOf(totalBeforeTip * 20 / 100));
-                    billTotalAfterTip[0] = totalBeforeTip + totalBeforeTip * 20 / 100;
-                    billTotalTextView.setText(String.valueOf(totalBeforeTip + totalBeforeTip * 20 / 100));
-                    billTotalPerPersonTextView.setText(String.valueOf(totalBeforeTip + totalBeforeTip * 20 / 100));
-                }
-            }
-        });
 
         Button addPersonButton = findViewById(R.id.button_plus);
         addPersonButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 personCountTextView.setText(String.valueOf(++personCount));
-                billTotalPerPersonTextView.setText(String.valueOf(billTotalAfterTip[0] / personCount));
-                tipTextView.setText(String.valueOf(totalBeforeTip[0] * 15 / 100 / personCount));
+                //billTotalPerPersonTextView.setText(String.valueOf(billTotalAfterTip[0] / personCount));
+                //tipTextView.setText(String.valueOf(totalBeforeTip[0] * 15 / 100 / personCount));
             }
         });
 
@@ -89,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if (personCount > 1) {
                     personCountTextView.setText(String.valueOf(--personCount));
-                    billTotalPerPersonTextView.setText(String.valueOf(billTotalAfterTip[0] / personCount));
+                   // billTotalPerPersonTextView.setText(String.valueOf(billTotalAfterTip[0] / personCount));
                 }
             }
         });
