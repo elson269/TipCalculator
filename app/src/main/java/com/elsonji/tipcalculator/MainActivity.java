e0af37e3783442b514fe0276f504c3754259a3d6
Elson <elson.ji@gmail.com>
2018-06-26 21:54:10 -0400

    feat: add computation functions.

--- app/src/main/java/com/elsonji/tipcalculator/MainActivity.java
+++ app/src/main/java/com/elsonji/tipcalculator/MainActivity.java
@@ -1,52 +1,86 @@
 package com.elsonji.tipcalculator;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.billAmountEditText) EditText billAmountEditText;
+    @BindView(R.id.tipAmountEditText) EditText tipAmountEditText;
+    @BindView(R.id.button_minus) Button minusButton;
+    @BindView(R.id.button_plus) Button plusButton;
+    @BindView(R.id.personCountTextView) TextView personCountTextView;
+    @BindView(R.id.fab) FloatingActionButton fab;
+    @BindView(R.id.totalAmountTextView) TextView totalAmountTextView;
+    @BindView(R.id.totalPersonAmountTextView) TextView totalPersonAmountTextView;
+    @BindView(R.id.tipAmountTextView) TextView tipAmountTextView;
+    @BindView(R.id.tipPersonAmountTextView) TextView tipPersonAmountTextView;
+
     int personCount = 1;
+    double billAmount;
+    double tipAmountPercent;
+    double totalAmount;
+    double totalPersonAmount;
+    double tipAmount;
+    double tipPersonAmount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        //final TextView tipTextView = findViewById(R.id.tipTextView);
-
-        final TextView personCountTextView = findViewById(R.id.personCountTextView);
-
-       // final TextView billTotalPerPersonTextView = findViewById(R.id.totalPerPerson);
-
-        final double[] billTotalAfterTip = new double[1];
-
-        final double[] totalBeforeTip = new double[1];
-
-
-
-        Button addPersonButton = findViewById(R.id.button_plus);
-        addPersonButton.setOnClickListener(new View.OnClickListener() {
+        plusButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 personCountTextView.setText(String.valueOf(++personCount));
-                //billTotalPerPersonTextView.setText(String.valueOf(billTotalAfterTip[0] / personCount));
-                //tipTextView.setText(String.valueOf(totalBeforeTip[0] * 15 / 100 / personCount));
             }
         });
 
-
-        Button minusPersonButton = findViewById(R.id.button_minus);
-        minusPersonButton.setOnClickListener(new View.OnClickListener() {
+        minusButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (personCount > 1) {
-                    personCountTextView.setText(String.valueOf(--personCount));
-                   // billTotalPerPersonTextView.setText(String.valueOf(billTotalAfterTip[0] / personCount));
-                }
+                if (personCount > 1)
+                personCountTextView.setText(String.valueOf(--personCount));
             }
         });
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String billAmountString = billAmountEditText.getText().toString().trim();
+                if (billAmountString.matches("[0-9]+")) {
+                    billAmount = Double.parseDouble(billAmountString);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Numbers only!", Toast.LENGTH_LONG).show();
+                }
+
+                String tipAmountString = tipAmountEditText.getText().toString().trim();
+                if (tipAmountString.matches("[0-9]+")) {
+                    tipAmountPercent = Double.parseDouble(tipAmountString);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Numbers only!", Toast.LENGTH_LONG).show();
+                }
+                 tipAmount = billAmount * tipAmountPercent / 100;
+                 tipPersonAmount = tipAmount / personCount;
+                 totalAmount = billAmount + tipAmount;
+                 totalPersonAmount = totalAmount / personCount;
+
+                 tipAmountTextView.setText(String.valueOf(tipAmount));
+                 tipPersonAmountTextView.setText(String.valueOf(tipPersonAmount));
+                 totalAmountTextView.setText(String.valueOf(totalAmount));
+                 totalPersonAmountTextView.setText(String.valueOf(totalPersonAmount));
+            }
+        });
+
     }
 }
