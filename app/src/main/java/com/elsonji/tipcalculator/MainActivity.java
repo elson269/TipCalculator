2b77e84ff2667a17ac6664fd784961f041a3402c
Elson <elson.ji@gmail.com>
2018-07-28 20:10:45 -0400

    feat: add settings activity

--- app/src/main/java/com/elsonji/tipcalculator/MainActivity.java
+++ app/src/main/java/com/elsonji/tipcalculator/MainActivity.java
@@ -5,15 +5,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridLayout;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,7 +27,8 @@ import android.support.v7.widget.Toolbar;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SharedPreferences.
+        OnSharedPreferenceChangeListener {
 
     @BindView(R.id.billAmountEditText)
     EditText billAmountEditText;
@@ -44,6 +50,12 @@ public class MainActivity extends AppCompatActivity {
     TextView tipAmountTextView;
     @BindView(R.id.tipPersonAmountTextView)
     TextView tipPersonAmountTextView;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+    @BindView(R.id.nav_view)
+    NavigationView mNavigationView;
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
 
     private int personCount = 1;
     private double billAmount;
@@ -52,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
     private double totalPersonAmount;
     private double tipAmount;
     private double tipPersonAmount;
-    private float defaultTip;
+    private boolean roundTip;
 
     private InputMethodManager mManager;
 
@@ -60,8 +72,6 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean mFabClickedStatus = false;
 
-    private SharedPreferences mDefaultTipPref;
-    private SharedPreferences.Editor mDefaultTipEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,11 +79,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        mDefaultTipPref = getSharedPreferences(getString(R.string.preference_default_tip_key), MODE_PRIVATE);
-        defaultTip = mDefaultTipPref.getFloat(getString(R.string.default_tip_key), 0);
-        if (defaultTip != 0) {
-            tipAmountEditText.setText(String.valueOf(defaultTip));
-        }
+        setSupportActionBar(mToolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        //Register the listener.
+        PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
+        ;
+
+        mNavigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        item.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        Intent settingsActivityIntent = new Intent(getApplication(), SettingsActivity.class);
+                        startActivity(settingsActivityIntent);
+                        return true;
+                    }
+                }
+        );
 
         mTipViewModel = ViewModelProviders.of(this).get(TipViewModel.class);
         displayPersonCount();
@@ -138,27 +164,43 @@ public class MainActivity extends AppCompatActivity {
 
                 calculateTip(billAmount, tipAmountPercent);
             }
-
         });
-
     }
 
     private void displayPersonCount() {
         personCount = mTipViewModel.getPersonCount();
-        personCountTextView.setText(String.valueOf(personCount) );
+        personCountTextView.setText(String.valueOf(personCount));
     }
 
     private void calculateTip(double billAmount, double tipAmountPercent) {
+        double roundedTipAmount, roundedTipPersonAmount, roundedTotalAmount, roundedTotalPersonAmount;
 
         tipAmount = billAmount * tipAmountPercent / 100;
         tipPersonAmount = tipAmount / personCount;
         totalAmount = billAmount + tipAmount;
         totalPersonAmount = totalAmount / personCount;
 
-        tipAmountTextView.setText(String.valueOf(Math.round(tipAmount * 100d) / 100d));
-        tipPersonAmountTextView.setText(String.valueOf(Math.round(tipPersonAmount * 100d) / 100d));
-        totalAmountTextView.setText(String.valueOf(Math.round(totalAmount * 100d) / 100d));
-        totalPersonAmountTextView.setText(String.valueOf(Math.round(totalPersonAmount * 100d) / 100d));
+        roundedTipAmount = Math.round(tipAmount * 100d) / 100d;
+        roundedTipPersonAmount = Math.round(tipPersonAmount * 100d) / 100d;
+        roundedTotalAmount = Math.round(totalAmount * 100d) / 100d;
+        roundedTotalPersonAmount = Math.round(totalPersonAmount * 100d) / 100d;
+
+        if (!roundTip) {
+            tipAmountTextView.setText(String.valueOf(roundedTipAmount));
+            tipPersonAmountTextView.setText(String.valueOf(roundedTipPersonAmount));
+            totalAmountTextView.setText(String.valueOf(roundedTotalAmount));
+            totalPersonAmountTextView.setText(String.valueOf(roundedTotalPersonAmount));
+        } else {
+            int integerTotalEach = (int) Math.round(roundedTotalPersonAmount);
+            double difference = integerTotalEach - roundedTotalPersonAmount;
+            double roundedDiff = Math.round(difference * 100d) / 100d;
+            roundedTipPersonAmount = roundedTipPersonAmount + roundedDiff;
+
+            tipAmountTextView.setText(String.valueOf(roundedTipAmount));
+            tipPersonAmountTextView.setText(String.valueOf(roundedTipPersonAmount));
+            totalAmountTextView.setText(String.valueOf(roundedTotalAmount));
+            totalPersonAmountTextView.setText(String.valueOf(integerTotalEach));
+        }
     }
 
     @Override
@@ -170,10 +212,39 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int itemClicked = item.getItemId();
-        if (itemClicked == R.id.action_default_tip) {
-            Intent defaultTipActivityIntent = new Intent(this, DefaultTipActivity.class);
-            startActivity(defaultTipActivityIntent);
+
+        if (itemClicked == android.R.id.home) {
+            mDrawerLayout.openDrawer(GravityCompat.START);
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(getString(R.string.pref_default_tip_key))) {
+            String defaultTip = sharedPreferences.getString(getString(R.string.pref_default_tip_key), "");
+            tipAmountEditText.setText(defaultTip);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        String defaultTip = sharedPref.getString(getString(R.string.pref_default_tip_key), "");
+        tipAmountEditText.setText(defaultTip);
+
+        roundTip = sharedPref.getBoolean(getString(R.string.pref_round_tip_key), false);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Unregister VisualizerActivity as an OnPreferenceChangedListener to avoid any memory leaks.
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+
 }
