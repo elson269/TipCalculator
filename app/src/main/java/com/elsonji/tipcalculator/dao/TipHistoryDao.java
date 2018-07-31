568435a82f2823a1f36bc5241ae043012eca2ad8
Elson <elson.ji@gmail.com>
2018-07-30 23:20:11 -0400

    feat: implementing Room

---
+++ app/src/main/java/com/elsonji/tipcalculator/dao/TipHistoryDao.java
@@ -0,0 +1,24 @@
+package com.elsonji.tipcalculator.dao;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import com.elsonji.tipcalculator.entity.TipHistory;
+
+import java.util.List;
+
+@Dao
+public interface TipHistoryDao {
+    @Query("SELECT * FROM tip_history_table")
+    LiveData<List<TipHistory>> getAllHistory();
+
+    @Insert
+    void insertHistory(TipHistory... tipHistory);
+
+    @Delete
+    void deleteHistories(TipHistory...tipHistories);
+
+}
---
+++ app/src/main/java/com/elsonji/tipcalculator/entity/TipHistory.java
@@ -0,0 +1,58 @@
+package com.elsonji.tipcalculator.entity;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "tip_history_table")
+public class TipHistory {
+    @PrimaryKey(autoGenerate = true)
+    private int mId;
+
+    @NonNull
+    @ColumnInfo(name = "date")
+    private String mDate;
+
+    @ColumnInfo(name = "bill_amount")
+    private double mBillAmount;
+
+    @ColumnInfo(name = "tip_percent")
+    private int mTipPercent;
+
+    @ColumnInfo(name = "tip_per_person")
+    private double mTipPerPerson;
+
+    @ColumnInfo(name = "total_per_person")
+    private double mTotalPerPerson;
+
+    public TipHistory( String date, double billAmount, int tipPercent, double tipPerPerson,
+                      double totalPerPerson) {
+        mDate = date;
+        mBillAmount = billAmount;
+        mTipPercent = tipPercent;
+        mTipPerPerson = tipPerPerson;
+        mTotalPerPerson = totalPerPerson;
+    }
+
+    public String getTipDate() {
+        return mDate;
+    }
+
+    public double getBillAmount() {
+        return mBillAmount;
+    }
+
+    public int getTipPercent() {
+        return mTipPercent;
+    }
+
+    public double getTipPerPerson() {
+        return mTipPerPerson;
+    }
+
+    public double getTotalPerPerson() {
+        return mTotalPerPerson;
+    }
+
+}
