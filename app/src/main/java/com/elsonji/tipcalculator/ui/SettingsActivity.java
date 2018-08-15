461d5f1568f8a8c12e875a6575e5afbd4c3a5aa8
Elson <elson.ji@gmail.com>
2018-08-14 23:09:11 -0400

    Feat: create customized preference dialog.

--- app/src/main/java/com/elsonji/tipcalculator/ui/SettingsActivity.java
+++ app/src/main/java/com/elsonji/tipcalculator/ui/SettingsActivity.java
@@ -2,15 +2,13 @@ package com.elsonji.tipcalculator.ui;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.NavUtils;
-import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 
 import com.elsonji.tipcalculator.R;
 
 public class SettingsActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
